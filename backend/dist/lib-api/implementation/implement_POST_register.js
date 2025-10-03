f5887e0063f4753e83e3a6edc7cbe94a8bc0c6ad
Pristya Haliza <you@example.com>
2025-10-03 16:59:20 +0700

    register backend is available

--- backend/dist/lib-api/implementation/implement_POST_register.js
+++ backend/dist/lib-api/implementation/implement_POST_register.js
@@ -8,16 +8,47 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.implement_POST_register = implement_POST_register;
+const User_1 = require("../model/table/User");
+const bcrypt_1 = __importDefault(require("bcrypt"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 function implement_POST_register(engine) {
     engine.implement({
         endpoint: 'POST /register',
         fn(param) {
             return __awaiter(this, void 0, void 0, function* () {
-                // register user dengan  username,  password
-                return {};
+                const { username, password, fullname, role } = param.body;
+                // cek apakah username sudah dipakai
+                const existing = yield User_1.User.findOne({ where: { username } });
+                if (existing) {
+                    return Promise.reject({
+                        status: 400,
+                        message: "Username sudah dipakai",
+                    });
+                }
+                // Asumsi JWT_SECRET didefinisikan di suatu tempat (misalnya dari process.env)
+                const JWT_SECRET = process.env.JWT_SECRET || 'kunci_rahasia_yang_sangat_aman_dan_panjang_sekali_123!@#';
+                // hash password
+                const hashedPassword = yield bcrypt_1.default.hash(password, 10);
+                // buat user baru
+                const newUser = User_1.User.create({
+                    username,
+                    password: hashedPassword,
+                    fullname,
+                    role,
+                    created_at: new Date(),
+                });
+                yield User_1.User.save(newUser);
+                const jwtToken = jsonwebtoken_1.default.sign({ id: newUser.id, username: newUser.username }, JWT_SECRET, { expiresIn: '1d' });
+                return {
+                    token: jwtToken,
+                    user: newUser
+                };
             });
-        }
+        },
     });
 }
