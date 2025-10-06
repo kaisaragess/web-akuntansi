dbe8a9a2aa8ba3655cbba3c93a05f82fd33d3221
kaisar ages <kaisar.agesquerio@gmail.com>
2025-10-06 15:08:39 +0700

    Ada perubahan skema journalRes, tabel Journals, Mengisi Post Journal

--- backend/lib-api/ts-model/table/Journals.ts
+++ backend/lib-api/ts-model/table/Journals.ts
@@ -13,7 +13,7 @@ export class Journals {
   @IsISO8601({}, { message: 'date must be an ISO8601 date' })
   date!: Date
   @IsString({ message: 'description must be a string' })
-  description!: string
+  description?: string
   @IsString({ message: 'referensi must be a string' })
   referensi?: string
   @IsString({ message: 'lampiran must be a string' })
