31928b97305532461bfc20fd37f1c0f218e2ac68
kaisar ages <kaisar.agesquerio@gmail.com>
2025-10-10 16:09:48 +0700

    tambah frontend ke project local

--- frontend/src/app/user/dashboard/page.tsx
+++ frontend/src/app/user/dashboard/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState, useEffect } from "react";
 import Sidebar from "@/app/components/Sidebar/page";
-import Navbar from "@/app/components/Navbar/page";
 
 const Dashboard = () => {
   return (
