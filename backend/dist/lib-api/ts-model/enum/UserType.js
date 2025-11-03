2a0ff1e1cb038d76dd4b12b765b10e6cbfc223a7
kaisar ages <kaisar.agesquerio@gmail.com>
2025-11-03 16:52:11 +0700

    hapus dist

--- backend/dist/lib-api/ts-model/enum/UserType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserType = void 0;
-var UserType;
-(function (UserType) {
-    UserType["admin"] = "admin";
-    UserType["user"] = "user";
-})(UserType || (exports.UserType = UserType = {}));
-;
--- backend/dist/lib-api/ts-model/enum/normal_balance.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.normal_balance = void 0;
-var normal_balance;
-(function (normal_balance) {
-    normal_balance["DEBIT"] = "DEBIT";
-    normal_balance["KREDIT"] = "KREDIT";
-})(normal_balance || (exports.normal_balance = normal_balance = {}));
-;
